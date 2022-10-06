3322566d1d80c1849e2e8cd1392e0aaf15edd53b
yota ogihara <sponge3039@gmail.com>
2022-10-06 15:48:16 +0900

    CMS

--- pages/libs/client.js
+++ pages/libs/client.js
@@ -1,7 +1,7 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-    erviceDomain : process.env.SERVIS_DOMAIN,
+    serviceDomain : process.env.SERVIS_DOMAIN,
     apiKey : process.env.API_KEY,
 });
 
