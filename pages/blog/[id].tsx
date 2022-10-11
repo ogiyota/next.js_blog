4b56773d3db1aea4e0c27b8ff6f7f800293608cc
yota ogihara <sponge3039@gmail.com>
2022-10-11 14:56:22 +0900

    ddddf

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -41,7 +41,7 @@ export default function BlogId( {blog}:any ) {
             <div className="back">
               <Link href={"#"}>
                 <a onClick={() => Router.back() }>
-                    ←戻るaa
+                    ←戻るa
                 </a>
               </Link> 
             </div>
